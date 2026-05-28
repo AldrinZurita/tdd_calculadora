2d6a9191042ea68f549c7b8733366d20541c29de
AldrinZurita <axzt2004@gmail.com>
2026-05-27 21:48:28 -0400

    refactor: agregar comentarios para mejorar legibilidad

--- src/sumarCadena.js
+++ src/sumarCadena.js
@@ -1,12 +1,15 @@
 function sumarCadena(cadena) {
+  // Cadena vacía retorna 0
   if (cadena === "") {
     return 0;
   }
   
+  // Parsear y retornar el número
   return parseInt(cadena, 10);
 }
 
 export default sumarCadena;
+
 
 
 
